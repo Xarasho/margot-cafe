7d14c0ea0f8ddaffa212b93bbed49ff468c2c452
Xarasho <alex.olmedo.rojas@gmail.com>
2026-04-29 23:36:02 -0600

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,50 @@
+import Button from "@/components/Button";
+import { ScrollView, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 30,
+      }}
+    >
+      <Text>Margot Cafe</Text>
+      <Button
+        onPress={() => alert("Quieres un cafe?")}
+        size="sm"
+        disabled
+        variant="danger"
+      >
+        Haz Click
+      </Button>
+      <Button onPress={() => alert("Quieres un cafe?")} variant="sunset">
+        Haz Click
+      </Button>
+      <Button onPress={() => alert("Quieres un cafe?")} size="lg">
+        Haz Click
+      </Button>
+      <Button size="lg" loading variant="danger">
+        Haz Click
+      </Button>
+      <Button
+        onPress={() => alert("Quieres un cafe?")}
+        size="lg"
+        style={{ backgroundColor: "purple" }}
+        textStyle={{ color: "yellow" }}
+      >
+        Haz Click
+      </Button>
+      <Button
+        onPress={() => alert("Quieres un cafe?")}
+        size="lg"
+        style={{ backgroundColor: "green" }}
+        textStyle={{ color: "white" }}
+      >
+        Haz Click
+      </Button>
+    </ScrollView>
+  );
+}
---
+++ components/Button.tsx
@@ -0,0 +1,98 @@
+import * as React from "react";
+import {
+  ActivityIndicator,
+  Pressable,
+  Text,
+  TextStyle,
+  ViewStyle,
+} from "react-native";
+
+type ButtonVariants = "sunset" | "ghost" | "danger";
+type ButtonSize = "sm" | "md" | "lg";
+
+interface ButtonProps {
+  onPress?: () => void;
+  children?: React.ReactNode;
+  disabled?: boolean;
+  loading?: boolean;
+  size?: ButtonSize;
+  variant?: ButtonVariants;
+  style?: ViewStyle;
+  textStyle?: TextStyle;
+}
+
+export default function Button({
+  onPress,
+  children,
+  disabled = false,
+  loading = false,
+  size = "md",
+  variant,
+  style,
+  textStyle,
+}: ButtonProps) {
+  const sizeStyles: Record<
+    ButtonSize,
+    { height: number; fontSize: number; paddingHorizontal: number }
+  > = {
+    sm: { height: 36, fontSize: 14, paddingHorizontal: 12 },
+    md: { height: 44, fontSize: 16, paddingHorizontal: 16 },
+    lg: { height: 55, fontSize: 18, paddingHorizontal: 20 },
+  };
+
+  function getTextColor(): string {
+    switch (variant) {
+      case "sunset":
+        return "#fff";
+      case "danger":
+        return "black";
+      default:
+        return "#fff";
+    }
+  }
+
+  function getVariantStyles(): ViewStyle {
+    switch (variant) {
+      case "sunset":
+        return { backgroundColor: "#D17842" };
+      case "danger":
+        return {
+          backgroundColor: "#fff",
+          borderWidth: 1,
+          borderColor: "black",
+        };
+      default:
+        return { backgroundColor: "#252A32" };
+    }
+  }
+
+  return (
+    <Pressable
+      onPress={onPress}
+      disabled={disabled || loading}
+      style={[
+        getVariantStyles(),
+        {
+          height: sizeStyles[size].height,
+          paddingHorizontal: sizeStyles[size].paddingHorizontal,
+          // backgroundColor: disabled ? "#52555A" : "#D17842",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 12,
+          opacity: disabled || loading ? 0.3 : 1,
+        },
+        style,
+      ]}
+    >
+      <Text
+        style={[
+          { fontSize: sizeStyles[size].fontSize, color: getTextColor() },
+          textStyle,
+        ]}
+      >
+        {loading ? <ActivityIndicator /> : children}
+      </Text>
+    </Pressable>
+  );
+}
